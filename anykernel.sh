a36015ae75c444069f740d78be95d1ea8d659c83
osm0sis <osm0sis@outlook.com>
2021-06-08 01:17:17 -0300

    Example: add commented vendor_boot setup for reference
    DO NOT MERGE if you have already updated your ramdisk properly for your device
    
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -30,7 +30,7 @@ ramdisk_compression=auto;
 # set permissions/ownership for included ramdisk files
 
 
-## AnyKernel install
+## AnyKernel boot install
 dump_boot;
 
 # begin ramdisk changes
@@ -44,5 +44,21 @@ dump_boot;
 # end ramdisk changes
 
 write_boot;
-## end install
+## end boot install
+
+
+# shell variables
+#block=vendor_boot;
+#is_slot_device=1;
+#ramdisk_compression=auto;
+
+# reset for vendor_boot patching
+#reset_ak;
+
+
+## AnyKernel vendor_boot install
+#split_boot; # skip unpack/repack ramdisk since we don't need vendor_ramdisk access
+
+#flash_boot;
+## end vendor_boot install
 
